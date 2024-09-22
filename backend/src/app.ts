0d65b11b868271141959e817b9d86d391481ab37
lauanS <lauan.s.souza@gmail.com>
2024-09-22 15:13:47 -0300

    chore: Add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import dbConnect from '@/config/dbConnect';
 import routes from '@/routes';
 const PORT = process.env.API_PORT;
@@ -6,6 +7,8 @@ const PORT = process.env.API_PORT;
 dbConnect();
 
 const app = express();
+
+app.use(cors());
 routes(app);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
