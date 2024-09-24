fb41dbb5fda37bfb2f0a6858ac3c0d6dbb0cbbab
lauanS <lauan.s.souza@gmail.com>
2024-09-23 23:25:24 -0300

    feat: Implements duplicate ID handling

--- backend/src/services/transation/process.ts
+++ backend/src/services/transation/process.ts
@@ -1,6 +1,37 @@
 import fs from 'fs';
 import readline from 'readline';
 import TransationRepository from '@/repositories/transations';
+import { registerIssueWithTransaction } from '@/utils';
+import { MongoError } from 'mongodb';
+
+const processLine = (line: string) => {
+  const [id, name, doc, date, value] = line
+    .split(';')
+    .map((item) => item.split(':')[1]);
+
+  TransationRepository.create({
+    id,
+    name,
+    doc,
+    date: new Date(date),
+    value: Number(value)
+  }).catch(error => {
+    const isDuplicatedKey = error instanceof MongoError && error.code === 11000;
+    if (isDuplicatedKey) {
+      return;
+    }
+
+    registerIssueWithTransaction(line);
+  });
+};
+
+const finishProcess = (filePath: string) => {
+  fs.unlink(filePath, (error) => {
+    if (error) {
+      console.log(`Error when deleting file ${filePath}`);
+    }
+  });
+};
 
 const processTransations = (filePath: string) => {
   const fileStream = fs.createReadStream(filePath);
@@ -10,26 +41,8 @@ const processTransations = (filePath: string) => {
     crlfDelay: Infinity,
   });
 
-  rl.on('line', (line) => {
-    const [id, name, doc, date, value] = line
-      .split(';')
-      .map((item) => item.split(':')[1]);
-
-    TransationRepository.create({
-      id,
-      name,
-      doc,
-      date: new Date(date),
-      value: Number(value)
-    });
-  });
-
-  rl.on('close', () => {
-    console.log(`
-      File ${filePath} processing completed.
-      todo: exclude file
-    `);
-  });
+  rl.on('line', (line) => processLine(line));
+  rl.on('close', () => finishProcess(filePath));
 };
 
 export default processTransations;
